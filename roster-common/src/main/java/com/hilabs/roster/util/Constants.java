e137706e82a3936a345be028261dc4ce0fcfe6bf
manikanta_hilabs <manikanta.challa@hilabs.com>
2022-10-07 12:59:32 +0530

    minor fix

--- roster-common/src/main/java/com/hilabs/roster/util/Constants.java
+++ roster-common/src/main/java/com/hilabs/roster/util/Constants.java
@@ -1,8 +1,8 @@
 package com.hilabs.roster.util;
 
 public class Constants {
-//    public static int ROSTER_INGESTION_IN_PROGRESS = 13;
-//    public static int ROSTER_INGESTION_VALIDATION_FAILED = 15;
-//    public static int ROSTER_INGESTION_FAILED = 17;
-//    public static int ROSTER_INGESTION_COMPLETED = 19;
+    public static int ROSTER_INGESTION_IN_PROGRESS = 13;
+    public static int ROSTER_INGESTION_VALIDATION_FAILED = 15;
+    public static int ROSTER_INGESTION_FAILED = 17;
+    public static int ROSTER_INGESTION_COMPLETED = 19;
 }
