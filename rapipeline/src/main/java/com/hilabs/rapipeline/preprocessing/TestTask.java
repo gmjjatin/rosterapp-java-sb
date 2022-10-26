b484234af045cebd7fa3967be8ceccc8112f67a3
manikanta_hilabs <manikanta.challa@hilabs.com>
2022-10-27 00:09:58 +0530

    minor fix

---
+++ rapipeline/src/main/java/com/hilabs/rapipeline/preprocessing/TestTask.java
@@ -0,0 +1,68 @@
+package com.hilabs.rapipeline.preprocessing;
+
+
+import com.google.gson.Gson;
+import com.hilabs.mcheck.model.Task;
+import com.hilabs.rapipeline.service.PreProcessingTaskService;
+import com.hilabs.roster.service.DartRASystemErrorsService;
+import liquibase.repackaged.org.apache.commons.lang3.exception.ExceptionUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.hilabs.rapipeline.service.PreProcessingTaskService.preProcessingRunningMap;
+import static com.hilabs.rapipeline.util.Utils.trimToNChars;
+
+@Slf4j
+public class TestTask extends Task {
+
+    private PreProcessingTaskService preProcessingTaskService;
+
+    private DartRASystemErrorsService dartRASystemErrorsService;
+
+    private static final Gson gson = new Gson();
+
+    public TestTask(Map<String, Object> taskData) {
+        super(taskData);
+    }
+
+    @Override
+    public void run() {
+        boolean isLongTask = System.currentTimeMillis() % 2 == 0;
+        log.info("TestTask stared for {} isLongTask {}", gson.toJson(getTaskData()), isLongTask);
+        try {
+            List<String> commands = new ArrayList<>();
+            commands.add("python");
+            commands.add("./test.py");
+            log.info("Running command {}", commands.stream().collect(Collectors.joining(" ")));
+            ProcessBuilder processBuilder = new ProcessBuilder(commands);
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder builder = new StringBuilder();
+            List<String> outputLines = new ArrayList<>();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                log.info(line);
+                outputLines.add(line);
+            }
+            reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            line = null;
+            while ((line = reader.readLine()) != null) {
+                log.info(line);
+                outputLines.add(line);
+            }
+           log.info("TestTask ended for {} isLongTask {}", gson.toJson(getTaskData()), isLongTask);
+        } catch (Exception | Error ex) {
+            log.error("Error in TestTask done for {} message {}", gson.toJson(getTaskData()),
+                    ex.getMessage());
+        }
+    }
+}
