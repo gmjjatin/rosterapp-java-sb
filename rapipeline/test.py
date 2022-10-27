11688ff7cf2c1927c9d3d4f250edcb9f5beb2b09
manikanta_hilabs <manikanta.challa@hilabs.com>
2022-10-27 12:00:57 +0530

    minor fix

--- rapipeline/test.py
+++ rapipeline/test.py
@@ -2,5 +2,5 @@ import sys;
 import time;
 time.sleep(30 * 1000)
 x = -1
-if x < 0:
-  raise Exception("Sorry, no numbers below zero")
+# if x < 0:
+#   raise Exception("Sorry, no numbers below zero")
