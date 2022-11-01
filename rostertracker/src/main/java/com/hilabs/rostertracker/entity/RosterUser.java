903690e8413bc27f237b34dc974852f24c05b10b
manikanta_hilabs <manikanta.challa@hilabs.com>
2022-11-01 18:31:05 +0530

    who column changes -wip

--- rostertracker/src/main/java/com/hilabs/rostertracker/entity/RosterUser.java
+++ rostertracker/src/main/java/com/hilabs/rostertracker/entity/RosterUser.java
@@ -1,21 +1,26 @@
-package com.hilabs.roster.entity;
+package com.hilabs.rostertracker.entity;
 
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Data
 @Entity
 @Table(name = "roster_user")
+@EntityListeners(AuditingEntityListener.class)
 public class RosterUser {
     @Id
     @Column(name = "USER_ID")
     private String userId;
+
     @Column(name = "PWD")
     @JsonIgnore
     private String password;
@@ -25,14 +30,21 @@ public class RosterUser {
     private String lastName;
     @Column(name = "ACTIVE_FLAG")
     private Integer activeFlag;
-    @Column(name = "CREATED_DATE")
-    private Date createdDate;
-    @Column(name = "CREATED_USER_ID")
-    private String createdUserId;
-    @Column(name = "UPDATED_DATE")
-    private Date updatedDate;
-    @Column(name = "UPDATED_USER_ID")
-    private String updatedUserId;
 
+    @Column(name = "CREATED_DATE")
+    @CreatedDate
+    private Date createdDate;
+
+    @Column(name = "CREATED_USER_ID")
+    @CreatedBy
+    private String createdUserId;
+
+    @Column(name = "UPDATED_DATE")
+    @LastModifiedDate
+    private Date updatedDate;
+
+    @Column(name = "UPDATED_USER_ID")
+    @LastModifiedBy
+    private String updatedUserId;
 }
 
--- rostertracker/src/main/java/com/hilabs/rostertracker/repository/UserRepository.java
+++ rostertracker/src/main/java/com/hilabs/rostertracker/repository/UserRepository.java
@@ -1,27 +1,28 @@
-//package com.hilabs.rostertracker.repository;
-//import java.util.List;
-//
-//import com.hilabs.roster.entity.RosterUser;
-//import org.springframework.data.jpa.repository.Modifying;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.data.repository.query.Param;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface UserRepository extends CrudRepository<RosterUser, String>{
-//    RosterUser findByUserId(String userId);
-//
-//    @Query(value = "SELECT * FROM roster_user where USER_ID=:userId AND ACTIVE_FLAG=1",
-//            nativeQuery = true)
-//    RosterUser findActiveUserByUserId(@Param("userId") String userId);
-//
-//    @Modifying
-//    @Query(value = "UPDATE roster_user SET PWD = :encodedPassword "
-//            + "WHERE USER_ID = :userId", nativeQuery = true)
-//    void updatePassword(@Param("userId") String userId, @Param("encodedPassword") String encodedPassword);
-//
-//    @Query(value = "SELECT du.USER_ID, du.FIRST_NAME ,du.LAST_NAME ,duur.ROLE_CD FROM roster_user du JOIN roster_user_x_user_role duur  "
-//            + "ON du.USER_ID = duur.USER_ID WHERE du.ACTIVE_FLAG = 1 ORDER BY du.USER_ID ASC", nativeQuery = true)
-//    List<Object[]> getUsersWithRoles();
-//}
+package com.hilabs.rostertracker.repository;
+
+import com.hilabs.rostertracker.entity.RosterUser;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends CrudRepository<RosterUser, String>{
+    RosterUser findByUserId(String userId);
+
+    @Query(value = "SELECT * FROM roster_user where USER_ID=:userId AND ACTIVE_FLAG=1",
+            nativeQuery = true)
+    RosterUser findActiveUserByUserId(@Param("userId") String userId);
+
+    @Modifying
+    @Query(value = "UPDATE roster_user SET PWD = :encodedPassword "
+            + "WHERE USER_ID = :userId", nativeQuery = true)
+    void updatePassword(@Param("userId") String userId, @Param("encodedPassword") String encodedPassword);
+
+    @Query(value = "SELECT du.USER_ID, du.FIRST_NAME ,du.LAST_NAME ,duur.ROLE_CD FROM roster_user du JOIN roster_user_x_user_role duur  "
+            + "ON du.USER_ID = duur.USER_ID WHERE du.ACTIVE_FLAG = 1 ORDER BY du.USER_ID ASC", nativeQuery = true)
+    List<Object[]> getUsersWithRoles();
+}
