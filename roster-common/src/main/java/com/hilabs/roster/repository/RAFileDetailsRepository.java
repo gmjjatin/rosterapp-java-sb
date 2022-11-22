1482bfb644ed32a639d638963f9fe7b79cec7472
manikanta_hilabs <manikanta.challa@hilabs.com>
2022-11-22 12:43:48 +0530

    release by bug fix

--- roster-common/src/main/java/com/hilabs/roster/repository/RAFileDetailsRepository.java
+++ roster-common/src/main/java/com/hilabs/roster/repository/RAFileDetailsRepository.java
@@ -1,7 +1,6 @@
 package com.hilabs.roster.repository;
 
 import com.hilabs.roster.entity.RAFileDetails;
-import com.hilabs.roster.entity.RASheetDetails;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Lock;
