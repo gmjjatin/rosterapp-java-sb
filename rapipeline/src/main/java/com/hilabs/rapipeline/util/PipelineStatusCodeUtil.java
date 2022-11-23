1c6e68a3b1aee0d83ce17d52001380a858a2fdbc
manikanta_hilabs <manikanta.challa@hilabs.com>
2022-11-23 22:30:30 +0530

    adding 31 to isf fecther and 41 to dart fetcher

--- rapipeline/src/main/java/com/hilabs/rapipeline/util/PipelineStatusCodeUtil.java
+++ rapipeline/src/main/java/com/hilabs/rapipeline/util/PipelineStatusCodeUtil.java
@@ -10,8 +10,8 @@ public class PipelineStatusCodeUtil {
     public static Integer preProcessingInQueueStatus = 20;
     public static List<Integer> preProcessingJob2StatusCodes = Arrays.asList(21);
     public static List<Integer> isfSheetStatusCodes = Arrays.asList(145);
-    public static List<Integer> isfFileStatusCodes = Arrays.asList(27);
-    public static List<Integer> dartStatusCodes = Arrays.asList(35);
+    public static List<Integer> isfFileStatusCodes = Arrays.asList(27, 31);
+    public static List<Integer> dartStatusCodes = Arrays.asList(35, 41);
 
 
     public static Integer dartUIValidationInProgressSheetStatusCode = 171;
